5b2b9ff86c6646c3746634e1d8595de3dd83801e
Christian velasco <christian.velasco@ucalgary.ca>
2018-10-08 11:11:51 -0600

    Compiler Scripted Updated
    - flags for file not in directory,
    - To compile do
       ./mac_compile.bash [filename]

--- Test Programs/mac_compile.bash
+++ Test Programs/mac_compile.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-./dasm.Darwin.x86 "$1" -o"$2".out
+if [ ! -e $1 ]; then 
+    echo " "
+    echo "*** File $1 DOES NOT EXITS ***"
+    echo " "
+
+else 
+    ./dasm.Darwin.x86 $1 -o${1%.*}.out
+    echo " "
+    echo "File Accepted: ${1%.*}.out in Directory "
+    echo "---"
+
+fi 
 
 
